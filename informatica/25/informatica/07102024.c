aa1666ffe26ba27ea83a6cde52e4e42df5a747e4
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-10-07 11:29:48 +0000

    ultimaciframenosignificativa

---
+++ informatica/25/informatica/07102024.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+        int main(){
+            int num, cifra;
+            num=0;
+            cifra=0;
+
+        
+            printf("inserire un numero: ");
+            scanf("%d", &num);
+
+            cifra=num%10;
+
+            if(cifra%2==0)
+                printf("il numero %d è pari", num);
+
+                else
+
+                printf("il numero %d è dispari", num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
