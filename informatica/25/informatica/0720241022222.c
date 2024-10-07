8078f02f8a59786b897912bc8613dc9265bdd634
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-10-07 11:58:08 +0000

    stabiliresedivisibileper3

---
+++ informatica/25/informatica/0720241022222.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/*456
+
+456%10=6   456/10=45
+45%10=5     45/10=4
+
+*/
+    int main(){
+        
+        int num=0;
+        int q, r;
+
+        printf("inserire un valore: ");
+        scanf("%d", &num);
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
