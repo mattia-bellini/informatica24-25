ac672fba694b5e97d7202a4e0c45f00c2095df52
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-10-10 20:27:38 +0000

    Compito

--- informatica/25/informatica/101024compito.c
+++ informatica/25/informatica/101024compito.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2, n3, n4, n5;
-    int sequenza_valida = 1;
-
-    printf("Inserisci 5 numeri interi:\n");
-
-    printf("Numero 1: ");
-    scanf("%d", &n1);
-    printf("Numero 2: ");
-    scanf("%d", &n2);
-    printf("Numero 3: ");
-    scanf("%d", &n3);
-    printf("Numero 4: ");
-    scanf("%d", &n4);
-    printf("Numero 5: ");
-    scanf("%d", &n5);
-
-    if (n2 - n1 != 1) {
-        sequenza_valida = 0;
-    } else if (n3 - n2 != 1) {
-        sequenza_valida = 0;
-    } else if (n4 - n3 != 1) {
-        sequenza_valida = 0;
-    } else if (n5 - n4 != 1) {
-        sequenza_valida = 0;
-    }
-
-    if (sequenza_valida) {
-        printf("La sequenza inserita e' una sequenza naturale valida.\n");
-    } else {
-        printf("La sequenza inserita non e' una sequenza naturale valida.\n");
-    }
-
-    return 0;
+    int n1;
+    int n2;
+    int n3;
+    int n4;
+    int n5;
+        printf("inserisci un numero");
+            scanf("%d", &n1);
+        printf("Inserisci un numero");
+            scanf("%d", &n2);
+        printf("Inserisci un numero");
+            scanf("%d", &n3);
+        printf("Inserisci un numero");
+            scanf("%d", &n4);
+        printf("Inserisci un numero");
+            scanf("%d", &n5);
+        if
+             (n1!=n2&&n1!=n3&&n1!=n4&&n1!=n5&&n2!=n3&&n2!=n4&&n2!=n5&&n3!=n4&&n3!=n5
+&&n4!=n5){
+if (n2 - n1 == 1 && n3 - n2 == 1 && n4 - n3 == 1 && n5 - n4
+== 1){
+            printf("La sequenza di numeri è valida");
+}
+            else{
+                printf("La sequenza non è valida");
+}
+}
+return 0;
 }
