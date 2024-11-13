3e5fe8b299afed91bc7cb6bfaaa57196578b6fac
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-11-13 08:05:25 +0000

    Compito

---
+++ informatica/25/informaticaEsercizi/whilenumgenerato.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    srand(time(NULL));
+    int n,n1=0;
+    n=random()%991+10;
+
+    while(n!=n1){
+        printf("inserisci un numero:",%n1)
+    }
+}
