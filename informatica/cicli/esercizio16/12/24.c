59561fae8dc86bf5e1c84bae87eb3e11abd22128
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-12-16 10:31:20 +0000

    fatto

--- informatica/cicli/esercizio16/12/24.c
+++ informatica/cicli/esercizio16/12/24.c
@@ -4,15 +4,16 @@ ES: 5-> 1+2+3+4+5
 
 #include <stdio.h>
 int main(){
-int n;
-printf("inserisci un numero: ")
+int n=0, somma=0;
+do{
+printf("inserisci un numero: ");
 scanf("%d",&n);
-    for(int i=0; i<=n; i++ ){
-        for()
+
+}while(n<0);
+    for(int i=1; i<=n; i++ ){
+        somma+=i;
+        printf("%d+", i);
     }
-
-
-
-
+    printf("\nla somma dei primi numeri naturali è %d", somma);
 
 }
