9538bb6bcc1c778bc504dd2d11bb4a523ecb1031
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2024-12-16 10:20:14 +0000

    eserici

---
+++ informatica/cicli/esercizio16/12/24.c
@@ -0,0 +1,18 @@
+/*calcolare la somma dei primi numeri naturali
+ES: 5-> 1+2+3+4+5
+*/
+
+#include <stdio.h>
+int main(){
+int n;
+printf("inserisci un numero: ")
+scanf("%d",&n);
+    for(int i=0; i<=n; i++ ){
+        for()
+    }
+
+
+
+
+
+}
