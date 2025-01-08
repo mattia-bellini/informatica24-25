30d80f404723386ea3150cbef42c66981c607111
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-01-08 08:44:40 +0000

    jnag

---
+++ informatica/cicli/vancanze.c
@@ -0,0 +1,11 @@
+/*Scrivi un programma per la rappresentazione del rettangolo “a spirale”, che contiene numeri naturali,
+ definito riempiendo le righe del rettangolo con numeri consecutivi e partendo da 1 nell’angolo in alto a sinistra.*/
+
+#include <stdio.h>
+int main(){
+int 
+
+
+
+}
+
