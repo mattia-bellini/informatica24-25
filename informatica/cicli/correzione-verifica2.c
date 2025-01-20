c4909a05fb99513bc300fd361231d319c14c8541
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-01-20 10:49:25 +0000

    correzione verifica

--- informatica/cicli/correzione-verifica2.c
+++ informatica/cicli/correzione-verifica2.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
-int main(){}
+int main(){
+    int num, cifra, pos=1, num_q;
+     do{
+         printf("inserisci un valore: ");
+            scanf("%d", &num);
+     }while(num<=0);
+         num_q=num;
 
+    while(num_q!=0){
+        cifra=num_q%10;
+            num_q=num_q/10;
+                if(cifra==pos)
+                printf("\n La cifra %d corrisponde alla sua posizione %d", cifra,pos);
+                    pos++;
 
-
-
-
+    }
+}
---
+++ informatica/cicli/correzione-verifica3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int num1, num2, i, sum1=0, sum2=0;
+do{
+    printf("inserisci due valori: ");
+    scanf("%d %d", &num1,&num2);
+}while(num1<=0 || num2<=0);
+
+for(i=1;i<=num1/2;i++){
+    if(num1%i==0)
+    sum1+=i;
+}
+for(i=1;i<=num2/2;i++){
+    if(num2%i==0)
+     sum2+=i;
+}
+if(sum1==num2 && sum2==num1)
+    printf("\n I due numeri sono amicali");
+else
+    printf("\n I due numeri non sono amicali");
+
+}
