af1e49fd38ce2ddb6caf1403c71dfbe03a7cd325
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-01-20 10:24:13 +0000

    adfghjkl

---
+++ esercitazione/es1.c
@@ -0,0 +1,22 @@
+ /*Date N coppie di numeri (con N generato casualmente con valori compresi tra 2 e 7) 
+ contare e stampare quelle che hanno i valori l'uno l'opposto dell'altro. (ad esempio 3 e -3 oppure -7 e 7)*/
+
+ #include <stdio.h>
+ #include<stdlib.h>
+ #include<time.h>
+
+int main(){
+int n;
+srand(time(NULL));
+n=rand()%6+2
+do{
+printf("inserisci un numero: ")
+scanf("%d", &n)
+
+}while(n>)
+
+
+
+
+
+}
---
+++ informatica/cicli/correzione-verifica.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main(){
+    int scelta, num, min_max, cnt=0;
+
+    printf("Scegli se la sequenza deve essere:\n ");
+    printf("1-Crescente\n");
+    printf("2-Decrescente\n");
+    printf("scelta:");
+    scanf("%d", &scelta);
+
+    printf("Inserisci il valore: ");
+    scanf("%d", &num);
+
+    min_max=num;
+    while(num!=0){
+        cnt++;
+        switch (scelta)
+    {
+        case 1:{
+            if(num>=min_max){
+                min_max=num;
+                printf("%d \n",min_max);
+            }
+        }
+            break;
+            case 2:{
+                if(num<=min_max){
+                    min_max=num;
+                    printf("%d \n", min_max);
+                }
+
+            }
+        
+        default:{
+            printf("Non hai eseguito la scelta giusta ");
+        }
+    printf("Inserisci un nuovo valore: ");
+    scanf("%d", &num);
+
+     }
+        }
+
+
+
+
+
+
+
+}
