a2bf90c74276bbc88830f911945feca6b991982d
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-02-17 10:55:08 +0000

    amicali

---
+++ informatica/funzioni/nuMamicali.c
@@ -0,0 +1,16 @@
+
+/*dati una sequenza di numeri letti a coppie si termina quando uno dei due valori è 0
+verificare se la coppia di numeri è amicale*/
+
+#include<stdio.h>
+
+int main(){
+    int num=1,num2=1;
+    do{
+        printf("Inserisci la coppia di numeri: , ", num,num2);
+        scanf("%d %d", &num,&num2);
+    }while(num)
+
+
+
+}
