d319b04c8a45e53373b4af6799a66ce80707f41b
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-03-31 09:54:54 +0000

    ciso ciwoo

---
+++ informatica/array/eseOgloio.c
@@ -0,0 +1,19 @@
+/*
+dopo aver caricato un vettore numerico confrontare
+ la somma delle componenti di indice oari con la somma delle componenti dispari
+e dire se la somma maggiore è divisibile 
+per un assegnato valore k diverso da 0
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "libreria.c"
+#include "libreria.h"
+#define DIM 5
+
+
+int main(){
+int v[DIM];
+ caricav(int v,dim)
+
+}
