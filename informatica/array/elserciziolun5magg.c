46d103cc287f9208d71c1a64b45b6aace381b65a
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-05-06 18:15:47 +0000

    mamamam

---
+++ informatica/array/elserciziolun5magg.c
@@ -0,0 +1,51 @@
+/*dato un array ordinato inserire un nuovo lemento all'interno dell'array*/
+/* Dato un array ordinato, inserire un nuovo elemento all'interno dell'array mantenendo l'ordine */
+
+#include <stdio.h>
+
+#define DIM 6 // Dimensione massima dell'array (incluso il nuovo elemento)
+
+// Funzione per stampare l'array
+void stampaArray(int arr[], int dim) {
+    for (int i = 0; i < dim; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Funzione per inserire un elemento in un array ordinato
+void inserisciElemento(int arr[], int *dim, int elemento) {
+    int i = *dim - 1;
+
+    // Sposta gli elementi verso destra per fare spazio al nuovo elemento
+    while (i >= 0 && arr[i] > elemento) {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+
+    // Inserisce il nuovo elemento nella posizione corretta
+    arr[i + 1] = elemento;
+
+    // Incrementa la dimensione dell'array
+    (*dim)++;
+}
+
+int main() {
+    int array[DIM] = {1, 3, 5, 7, 9}; // Array inizialmente ordinato
+    int dim = 5; // Dimensione attuale dell'array
+    int nuovoElemento;
+
+    printf("Array iniziale: ");
+    stampaArray(array, dim);
+
+    printf("Inserisci un nuovo elemento da aggiungere all'array: ");
+    scanf("%d", &nuovoElemento);
+
+    // Inserisce il nuovo elemento mantenendo l'ordine
+    inserisciElemento(array, &dim, nuovoElemento);
+
+    printf("Array dopo l'inserimento: ");
+    stampaArray(array, dim);
+
+    return 0;
+}
--- informatica/array/menuarray.c
+++ informatica/array/menuarray.c
@@ -1,14 +1,15 @@
 /*
 Creiamo un menu:
-1)caricare un array con soli valori pari
-2)stampare l'array
-3)ordina l'array con il bubble sort
+1) Caricare un array con soli valori pari
+2) Stampare l'array
+3) Ordinare l'array con il bubble sort
+4) Trovare il valore massimo all'interno dell'array e scambiarlo di posizione con il valore minimo
 */
 
 #include <stdio.h>
 #define DIM 5
 
-
+// Funzione per richiedere valori pari
 void richiediValori(int vett[], int dim) {
     int n = 0, i = 0;
     while (i < dim) {
@@ -23,14 +24,14 @@ void richiediValori(int vett[], int dim) {
     }
 }
 
-
+// Funzione per stampare l'array
 void stampaVett(int vett[], int dim, char sep) {
     for (int i = 0; i < dim; i++) {
         printf("%d%c", vett[i], (i == dim - 1) ? '\n' : sep);
     }
 }
 
-
+// Funzione per ordinare l'array con il bubble sort
 void bubbleSort(int vett[], int dim) {
     int temp;
     for (int i = 0; i < dim - 1; i++) {
@@ -44,10 +45,31 @@ void bubbleSort(int vett[], int dim) {
     }
 }
 
+// Funzione per scambiare il valore massimo con il valore minimo
+void scambioMinMax(int vett[], int dim) {
+    int min = vett[0], max = vett[0];
+    int i_min = 0, i_max = 0;
+
+    // Trova gli indici del minimo e del massimo
+    for (int i = 1; i < dim; i++) {
+        if (vett[i] < min) {
+            min = vett[i];
+            i_min = i;
+        }
+        if (vett[i] > max) {
+            max = vett[i];
+            i_max = i;
+        }
+    }
+
+    // Scambia i valori
+    int temp = vett[i_min];
+    vett[i_min] = vett[i_max];
+    vett[i_max] = temp;
+}
+
 int main() {
     int scelta;
-
-    
 
     if (DIM <= 0) {
         printf("La dimensione dell'array deve essere maggiore di 0.\n");
@@ -61,7 +83,8 @@ int main() {
         printf("1) Caricare un array con soli valori pari\n");
         printf("2) Stampare l'array\n");
         printf("3) Ordinare l'array con il bubble sort\n");
-        printf("4) Esci\n");
+        printf("4) Scambiare il valore massimo con il valore minimo\n");
+        printf("5) Esci\n");
         printf("Scegli un'opzione: ");
         scanf("%d", &scelta);
 
@@ -74,14 +97,20 @@ int main() {
                 break;
             case 3:
                 bubbleSort(vettore, DIM);
+                printf("Array ordinato con successo.\n");
                 break;
             case 4:
-                printf("Uscita dal programma\n");
+                scambioMinMax(vettore, DIM);
+                printf("Scambio effettuato con successo.\n");
+                stampaVett(vettore, DIM, '-');
+                break;
+            case 5:
+                printf("Uscita dal programma.\n");
                 break;
             default:
                 printf("Opzione non valida. Riprova.\n");
         }
-    } while (scelta != 4 && scelta!=0);
+    } while (scelta != 5);
 
     return 0;
 }
