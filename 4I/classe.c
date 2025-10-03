dc3b6f55acbce5f2201f1659c47ddf19325b29b0
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-10-03 09:57:23 +0000

    bhuobvj

---
+++ 4I/classe.c
@@ -0,0 +1,7 @@
+/*scrivi un programma in c che chieda all'utente un numero intero N compreso tra 1 e 26,
+ quindi crea una stringa composta da tutte le prime N lettere dell'alfabeto.
+ esempio: se l'utente inserisce 4, verrà creata la stringa "ABCD".
+ */
+#include <stdio.h>
+#include <stdlib.h> 
+
