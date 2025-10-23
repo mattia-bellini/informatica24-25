5f8766e3616963946bf203eaaea657ce8e19cc7c
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2025-10-23 06:58:32 +0000

    ciaso

---
+++ 4I/informatica/esrciozio21ottob.c
@@ -0,0 +1,23 @@
+/*scrivi un programma in c che elimini gli spazi da una stringa*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void rimuovi_spazi(char *s) {
+    int i;
+    for (i = 0; s[i] != '\0'; i++) {
+     if (s[i] != ' ') {   
+     
+        }
+
+               
+}
+
+
+
+
+}
+
+}
