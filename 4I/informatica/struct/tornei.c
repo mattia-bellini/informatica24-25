d1d6bf60729674b86aef46712cc6bcd1bab1e86e
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2026-04-14 07:11:33 +0000

    new

---
+++ 4I/informatica/struct/tornei.c
@@ -0,0 +1,20 @@
+/*si vuole creare un applicazione in c che possa gestire le partite di un torneo di istituto 
+.quindi si vuole registrare un elenco delle squadre che giocano a pallavolo: 
+l'utente dovrà inserire la classe e la sezione della squadra,
+un nome per la squadra e un elenco di 6 nomi che memorizzi i partecipanti*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
