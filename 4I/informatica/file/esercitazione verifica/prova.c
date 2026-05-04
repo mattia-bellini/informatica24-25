858124a46564ad0e3d98a6a0e5b755c740323661
mattia-bellini <mattia.bellini@galileiostiglia.edu.it>
2026-05-04 20:14:00 +0000

    vfgh

---
+++ 4I/informatica/file/esercitazione verifica/prova.c
@@ -0,0 +1,28 @@
+/*Scrivi un programma in linguaggio C che gestisca un file di testo contenente una lista di numeri interi.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void numeriInteri(){
+    FILE *fp=fopen("numeri.txt","wr")
+    if(fp==NULL){
+        printf("Errore nell'aprire il file");
+        return 1
+    }
+int n1,n2,n3;
+fprintf("INserisci il numero: %d\n", n);
+
+
+
+
+
+}
+
+
+
+
+
+int main(){
+
+
+}
